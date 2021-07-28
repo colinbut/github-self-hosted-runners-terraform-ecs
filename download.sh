d0df7d6c677c85434c4184926ab8e29f482c8718
Colin But <colin.but@outlook.com>
2021-07-28 22:53:47 +0100

    feat: download script to download runner app github

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# download github runner application
+curl -o actions-runner-linux-x64-2.278.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.278.0/actions-runner-linux-x64-2.278.0.tar.gz
+tar xzf actions-runner-linux-x64-2.278.0.tar.gz
