d88c73c5ba7ba40cabedd591c25288d6725b2fe3
Colin But <colin.but@outlook.com>
2021-08-07 23:52:54 +0100

    chore: remove unused var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,3 @@ variable "github_runner_pat_token" {
   description = "The PAT token of the GitHub user used to authenticate against the GitHub Repository for which to setup GitHub Self Hosted Runners for"
   type        = string
 }
-
-variable "github_runner_docker_image" {
-  description = "value"
-  type        = string
-}
